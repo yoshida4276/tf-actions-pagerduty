7d3de2a59af563f40721e0b3beec727c231cd38a
yoshida4276 <manato.yoshida.76@dl-techx.com>
2026-05-27 17:18:55 +0900

    Update main.tf

--- pd-terraform/main.tf
+++ pd-terraform/main.tf
@@ -1,5 +1,7 @@
 resource pagerduty_user example_user {
-  name  = 北川 涼吾
-  email = ryougo.kitagawa.17@dl-techx.com
-  role  = user
+  
+  name  = var.username
+  email = var.email
+  role  = var.role
+
 }
